056a5c25edcda45ee0df874566d689f5052a491c
Constantinos Papadopoulos <pcostasgr@gmail.com>
2019-11-20 22:33:10 +0200

    Changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import {createStore} from "redux"
 import todoApp from './../reducers/reducers';
-import {getComponentDb} from './../mockdb';
+import {getComponentDb} from './mockdb';
 
 const store=createStore(todoApp,getComponentDb());
 
