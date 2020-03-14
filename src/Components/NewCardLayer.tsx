0041fb4d00108d538ff18d1f92d7d8037705b0c9
pcostasgr <pcostasgr@gmail.com>
2020-03-14 23:44:45 +0200

    Changes

--- src/Components/NewCardLayer.tsx
+++ src/Components/NewCardLayer.tsx
@@ -3,8 +3,8 @@ import { useDispatch } from "react-redux";
 import Button from "@material-ui/core/Button";
 import { TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import { boardFacade } from '../store/Repository';
-import { authenticationService as auth } from '../Model/Users';
+import { boardFacade } from "../store/Repository";
+import { authenticationService as auth } from "../Model/Users";
 type SomeProps = {
   listId: number;
 };
@@ -13,38 +13,40 @@ const NewCardLayerComp = (props: SomeProps) => {
   const { listId } = props;
 
   const buttonId = "NCBListId" + listId;
-  
-  const userid=auth.currentUserValue.userId;
 
-  const dispatch=useDispatch();
+  const userid = auth.currentUserValue.userId;
+
+  const dispatch = useDispatch();
 
   const [buttonVisible, setButtonVisible] = useState(true);
- 
+
   const [title, setTitle] = useState("");
- 
+
   const useButtonStyle = makeStyles({
     label: {
       textTransform: "none"
     },
     root: {
-        background:'red',
-        color:'white'
+      background: "red",
+      color: "white"
     }
   });
 
   const classes = useButtonStyle();
-  
-  const createNewCard=(cardTitle:string)=>{
-      dispatch(boardFacade.cardApi.insertCardApi({
-				cardid:-1,
-				cardtitle:cardTitle,
-				listid:listId,
-				userid:userid,
-				carddate:null,
-				listItems:[],
-				labelItems:[]
-			}));
-  }
+
+  const createNewCard = (cardTitle: string) => {
+    dispatch(
+      boardFacade.cardApi.insertCardApi({
+        cardid: -1,
+        cardtitle: cardTitle,
+        listid: listId,
+        userid: userid,
+        carddate: null,
+        listItems: [],
+        labelItems: []
+      })
+    );
+  };
 
   const renderButton = (): any => {
     return (
@@ -77,41 +79,43 @@ const NewCardLayerComp = (props: SomeProps) => {
                 multiline={true}
                 fullWidth={true}
                 onChange={(event: any) => {
-                    setTitle(event.target.value);
+                  setTitle(event.target.value);
                 }}
               ></TextField>
             </td>
           </tr>
-          <tr><td>
-            <Button
-                id={"SB"+buttonId}
+          <tr>
+            <td>
+              <Button
+                id={"SB" + buttonId}
                 variant="contained"
                 classes={{
-                    root: classes.root,
-                    label: classes.label
+                  root: classes.root,
+                  label: classes.label
                 }}
                 size="large"
                 onClick={(event: any) => {
-                    createNewCard(title);
-                    setButtonVisible(true);
+                  createNewCard(title);
+                  setButtonVisible(true);
                 }}
-                >
+              >
                 Save
-            </Button>
-            <Button
-                id={"EB"+buttonId}
+              </Button>
+              <Button
+                id={"EB" + buttonId}
                 variant="contained"
                 size="large"
                 classes={{
-                    label: classes.label,
+                  label: classes.label
                 }}
                 onClick={(event: any) => {
-                    setButtonVisible(true);
+                  setButtonVisible(true);
                 }}
-                >
+              >
                 X
-            </Button>
-          </td></tr>
+              </Button>
+            </td>
+          </tr>
         </tbody>
       </table>
     );
